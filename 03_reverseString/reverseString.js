1de2abcb5bb3a9e0f6ac184f0d2faf0cdc0baf6a
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-25 18:24:14 +0300

    string reversing Js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newString = '';
+    for (let i = str.length - 1; i >= 0; i--)
+    {
+        newString += str[i];
+    }   
+    return newString;
 };
+const stringLength = function(str){
+   return str.length;  
+};
+reverseString("hello"); //olleh
 
 // Do not edit below this line
 module.exports = reverseString;
